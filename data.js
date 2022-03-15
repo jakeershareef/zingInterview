4841cf3841d790ddf819af82c3c26f8f2c47ddc0
jakeershareef <36979961+jakeershareef@users.noreply.github.com>
2022-03-15 12:42:33 +0530

    models

--- data.js
+++ data.js
@@ -1,74 +1,19 @@
-const router = require('express').Router();
-const Data = require('../models/data');
+const mongoose = require('mongoose');
 
-router.post('/', async (req,res) => {
-    try {
-        const { username,email,mobile,gender } = req.body;
-        if(!(username&&email&&mobile&&gender)) {
-            return res.status(401).send('Please provide all the details');
-        }
-        const oldUser = await Data.findOne({email}) 
-        if(oldUser) {
-            return res.status(409).send('User Already Existed');
-        }
-        const savedUser = await Data.create({
-            username,
-            email,
-            mobile,
-            gender
-        })
-        return res.status(200).send(savedUser);
-    } catch(err)  {
-        return res.status(500).send(err)
+const UserSchema = new mongoose.Schema({
+    username:{
+        type : String, requuired : true
+        },
+   
+    email : {
+        type : String, required : true
+    },
+    mobile : {
+        type : Number, required : true
+    },
+    gender : {
+        type : String , required : true
     }
 })
-
-router.get('/', async (req,res) => {
-    try {
-        const users = await Data.find();
-        return res.status(200).send(users);
-    } catch(err) {
-        return res.status(500).send(err)
-    }
-});
-router.get('/:id', async (req,res) => {
-    try {
-      
-        const data1 = await Data.findById(req.params.id);
-        return res.status(200).json(data1);
-    } catch(err) {
-       return res.status(500).json(err);
-    }
-});
-
-router.delete('/:id', async (req,res) => {
-    try {
-        if(!(req.params&&req.params.id)) {
-            return res.status(400).json("Bad Request");
-        }
-   const deleteData = await Data.deleteOne({_id:req.params.id})
-   return res.status(200).json("deleted")
-    } catch(err) {
-        return res.status(500).json(err);
-    }
-});
-
-router.patch('/:id', async (req,res) => {
-    try {
-        if(!(req.params&&req.params.id)) {
-            return res.status(400).json("Bad request");
-        } 
-       if(ObjectId.isValid(_id)) {
-           if((string) (new ObjectId(_id)) === id)
-               return true;
-           
-           return false;
-       }
-        const update = await Data.updateOne({ _id:req.params.id },{ $set: {username:req.body.username}});
-        return res.status(200).json(update);
-    } catch(err) {
-        return res.status(500).json(err);
-    }
-});
-
-module.exports = router;
+const User = mongoose.model('Data',UserSchema)
+module.exports = User;
